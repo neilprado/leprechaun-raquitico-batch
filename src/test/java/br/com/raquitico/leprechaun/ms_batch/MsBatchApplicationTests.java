1ccd0d52e77c08b9cd72da0f7e6231b476af75b6
neilprado <neiljohn17@gmail.com>
2026-06-09 21:47:04 -0300

    first commit

---
+++ src/test/java/br/com/raquitico/leprechaun/ms_batch/MsBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raquitico.leprechaun.ms_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
